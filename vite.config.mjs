aafdaacb5d7f34349aac82b52d8c18e41c962fb8
Nahojoo <happyduck0806@gmail.com>
2025-06-05 20:08:14 +0900

    배포설정

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,6 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-toy-project1/',
   plugins: [
     VueRouter(),
     Vue({
