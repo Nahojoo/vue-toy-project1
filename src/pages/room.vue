e2ed2eaa7d105e3752b1e4176b59c46a32a76325
Nahojoo <happyduck0806@gmail.com>
2025-06-12 18:34:26 +0900

    사진 변경

--- src/pages/room.vue
+++ src/pages/room.vue
@@ -1,6 +1,6 @@
 <template>
   <ParallaxItem :desc="desc" :img-url="src" :page-title="title" />
-  <v-container>
+  <v-container class="pt-10">
     <GridLayout
       class="pt-8 pb-8"
       :custom-cols="6"
@@ -13,7 +13,7 @@
           :btn-second-text="'예약하기'"
           :btn-text="'자세히보기'"
           :desc="item.text"
-          :height="300"
+          :height="360"
           :img-url="item.imgUrl"
           :info="item.info"
           second
@@ -41,38 +41,38 @@
     },
     {
       id: 2,
-      imgUrl: 'https://images.unsplash.com/photo-1631049307264-da0ec9d70304?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', info: '4명 최대 | 100㎡', title: '프리미엄 스위트', subTitle: '스위트',
+      imgUrl: 'https://images.unsplash.com/photo-1631049552057-403cdb8f0658?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', info: '4명 최대 | 100㎡', title: '프리미엄 스위트', subTitle: '스위트',
       text: {
         bed: '킹사이즈 베드 1개', view: 'City or River View',
       },
     },
     {
       id: 3,
-      imgUrl: 'https://images.unsplash.com/photo-1631049307264-da0ec9d70304?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', info: '4명 최대 | 100㎡', title: '디럭스 스위트', subTitle: '스위트', text: {
+      imgUrl: 'https://images.unsplash.com/photo-1631048835388-46642de1582e?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', info: '4명 최대 | 100㎡', title: '디럭스 스위트', subTitle: '스위트', text: {
         bed: '킹사이즈 베드 1개', view: 'City or River View',
       },
     },
     {
       id: 4,
-      imgUrl: 'https://images.unsplash.com/photo-1631049307264-da0ec9d70304?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', info: '4명 최대 | 100㎡', title: '프리미엄', subTitle: '일반', text: {
+      imgUrl: 'https://images.unsplash.com/flagged/photo-1573168710865-2e4c680d921a?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', info: '4명 최대 | 100㎡', title: '프리미엄', subTitle: '일반', text: {
         bed: '킹사이즈 베드 1개', view: 'City or River View',
       },
     },
     {
       id: 5,
-      imgUrl: 'https://images.unsplash.com/photo-1631049307264-da0ec9d70304?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', info: '4명 최대 | 100㎡', title: '디럭스', subTitle: '일반', text: {
+      imgUrl: 'https://images.unsplash.com/photo-1631048648883-660ccd576581?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', info: '4명 최대 | 100㎡', title: '디럭스', subTitle: '일반', text: {
         bed: '킹사이즈 베드 1개 or 퀸사이즈 베드 2개', view: 'City or River View',
       },
     },
     {
       id: 6,
-      imgUrl: 'https://images.unsplash.com/photo-1631049307264-da0ec9d70304?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', info: '2명 최대 | 100㎡', title: '스탠다드 (1베드)', subTitle: '일반', text: {
+      imgUrl: 'https://images.unsplash.com/photo-1725962479542-1be0a6b0d444?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', info: '2명 최대 | 100㎡', title: '스탠다드 (1베드)', subTitle: '일반', text: {
         bed: '퀸사이즈 베드 1개', view: 'City or River View',
       },
     },
     {
       id: 7,
-      imgUrl: 'https://images.unsplash.com/photo-1631049307264-da0ec9d70304?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', info: '4명 최대 | 100㎡', title: '스탠다드 (2베드)', subTitle: '일반', text: {
+      imgUrl: 'https://images.unsplash.com/photo-1690266503084-8ec336397478?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', info: '4명 최대 | 100㎡', title: '스탠다드 (2베드)', subTitle: '일반', text: {
         bed: '퀸사이즈 베드 2개', view: 'City or River View',
       },
     },
