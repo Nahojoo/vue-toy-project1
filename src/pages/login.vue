f6532e7d3ff6ebd604067f3326ca42cacc956024
Nahojoo <happyduck0806@gmail.com>
2025-06-25 17:40:58 +0900

    로그인페이지

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>로그인</div>
+  <v-container class="d-flex flex-column align-center justify-center h-100">
+    <v-icon icon="mdi-progress-alert" size="x-large" />
+    <div class="text-h4 pt-4">진행중 입니다</div>
+  </v-container>
+
 </template>
 
 <script setup>
